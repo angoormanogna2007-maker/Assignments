d175c779e9b69e8e7483304b113d0b4dda8dbbdf
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-09 22:50:59 +0530

    Added variables.java

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args){
+        int p = 50;
+        double q = 38.9;
+        String r = "manogna";
+        boolean s = false;
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(r);
+        System.out.println(s);
+
+    }
+    
+}
