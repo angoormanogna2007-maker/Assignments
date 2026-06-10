1762bcd698dca3f0874d82896c30882835f3b4d6
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-10 21:40:42 +0530

    added datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,20 @@
+public class datatypes {
+    public static void main(String[] args){
+        byte score = 126;
+        short phonePrice = 29765;
+        int tvprice = 45000;
+        long phoneNumber = 6705782549l;
+        float discount = 15.5f;
+        double accountBalance = 1456.32;
+        char gender = 'F';
+        boolean isStudent = true;
+        System.out.println(score);
+        System.out.println(phonePrice);
+        System.out.println(tvprice);
+        System.out.println(phoneNumber);
+        System.out.println(discount);
+        System.out.println(accountBalance);
+        System.out.println(gender);
+        System.out.println(isStudent);
+    }
+}
