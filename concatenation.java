36f2bf30f0ff835b2bc42d878e287f8a6f419a3a
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-12 19:10:44 +0530

    added concatenation,java

---
+++ concatenation.java
@@ -0,0 +1,12 @@
+public class concatenation {
+    public static void main(String[] args){
+        int bikeprice = 125000;
+        long aadharNumber = 123456789013l;
+        short phoneprice = 30000;
+        float marks = 20;
+        char section = 'A';
+        byte age = 55;
+        boolean isopen = true;
+
+    }
+}
