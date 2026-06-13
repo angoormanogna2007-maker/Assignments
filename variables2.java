b83ffb72a031817555ee6a31248d0ffdc831c78a
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-13 12:39:41 +0530

    added variables.java

--- variables2.java
+++ variables2.java
@@ -1,17 +1,15 @@
 public class variables2 {
     static char section ='c';
-    static char initial = 'A';
+    static char initial ='A';
     static char grade = 'B';
-    static char Symbol = '@';
-    static char gender = 'F';
-    public static void main(String[] args){
-        char Symbol = '#';
-        char gender = 'm';
-        System.out.println(gender);
+   public  static void main(String[] args){
+        char symbol = '@';
+        char gender = 'f';
         System.out.println(section);
         System.out.println(initial);
         System.out.println(grade);
-        System.out.println(Symbol);
+        System.out.println(symbol);
+        System.out.println(gender);
+
     }
 }
-   
