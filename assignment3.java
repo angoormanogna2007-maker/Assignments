b39a32f8bf533c3e47bce25fbe99e7dc16b822b8
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-17 20:38:41 +0530

    added variables3.java

---
+++ assignment3.java
@@ -0,0 +1,19 @@
+public class assignment3
+{
+    static int marks = 50;   
+    static int price = 100;   
+    static int date = 13; 
+    static int rollNumbers= 21;
+ public static void main(String[] args){    
+   int marks = 69; 
+   int price = 150;
+   int age = 21;
+   
+    System.out.println(marks); 
+    System.out.println(price);    
+    System.out.println(date);  
+    System.out.println(rollNumbers);  
+    System.out.println(age);
+ }
+}  
+
