b91d2a5c5a06fb4fc2d70b3a862171767ff7e807
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-17 21:10:38 +0530

    added stringss.java

--- Stringss.java
+++ Stringss.java
@@ -1,4 +1,11 @@
 public class Stringss {
+    public static void main(String[] args){
+        String firstName = "Angoor";
+        String lastName = "manogna";
+
+        System.out.println("String: "+ firstName);
+        System.out.println("string: "+ lastName);
+   }
     
     
 }
