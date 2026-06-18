315b0401ea9de922305c7f1fbb6a823854cf5131
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-18 18:53:20 +0530

    added increment.java

---
+++ increment.java
@@ -0,0 +1,10 @@
+public class increment {
+    public static void main(String[] args){
+        int x = 5;
+        int y = ++x;
+        System.out.println(y);
+        System.out.println(x);
+
+    }
+    
+}
