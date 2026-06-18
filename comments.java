a50afec0c68a2df611f148409654c670b0856360
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-18 18:11:39 +0530

    added comments.java

---
+++ comments.java
@@ -0,0 +1,16 @@
+public class comments {
+    Name          : Manogna
+    father name   : Chandra shekar
+    Mother name   : Bhagya 
+    Date of birth : 05-05-2007
+    Gender        : female
+    Address       : Ram nagar,armoor
+    Mobile no     :1234567891
+    Email id      :angoormanogna@gmail.com
+    Education  :
+        schooling    :In ZPHS girls,armoor
+        intermediate : kshatriya junior college,armoor
+        Degree       : Nishitha degree college,Nizamabad
+
+
+}
