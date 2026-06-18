7fa0aa3e36ffa59f1ca11d59cb29986c01066be3
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-18 19:15:36 +0530

    added relational.java

---
+++ relational.java
@@ -0,0 +1,14 @@
+public class relational {
+    public static void main(String[] args){
+        int score = 70;
+        int passing = 35;
+        System.out.println(score==passing);
+        System.out.println(score!=passing);
+        System.out.println(score>passing);
+        System.out.println(score<passing);
+        System.out.println(score>=passing);
+        System.out.println(score<=passing);
+
+
+    }
+}
