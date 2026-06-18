99500e9e9544173b921b695382b4d491a35b918c
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-18 19:01:58 +0530

    added decrement.java

---
+++ decrement.java
@@ -0,0 +1,8 @@
+public class decrement {
+    public static void main(String[] args){
+        double a=22.5;
+        double b=a++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
