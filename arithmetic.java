74f4ecf5ee2448db5cca092a3d37d72315d165ba
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-18 18:31:39 +0530

    added arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+public class arithmetic {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        System.out.println(a+b);  //7
+        System.out.println(a-b);  //3
+        System.out.println(a*b);  //10
+        System.out.println(a/b);  //2
+        System.out.println(a%b);  //1
+
+    }
+}
+
