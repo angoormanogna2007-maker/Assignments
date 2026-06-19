f0671c9e6c45c1369504bc2d5a0abf1cac0a21b6
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-19 15:58:58 +0530

    added logical2.java

---
+++ logical2.java
@@ -0,0 +1,7 @@
+public class logical2 {
+    public static void main(String[] args){
+        boolean isConnected =false;
+        boolean isDisconnected =true;
+        System.out.println(isConnected & isDisconnected);
+    }
+}
