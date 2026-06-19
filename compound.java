65fcfb6523c71b3c09d55737966de55216209e16
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-19 16:36:32 +0530

    compound.java

---
+++ compound.java
@@ -0,0 +1,10 @@
+public class compound {
+    public static void main(String[] args){
+        int marks =70;
+
+
+        marks +=30;       // marks = 70 + 50 = 120
+        System.out.println(marks);   // 120
+
+    }
+}
