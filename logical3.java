a0c55ca93f15122faa194295d371cb0eddf07699
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-19 16:06:12 +0530

    added logical3.java

---
+++ logical3.java
@@ -0,0 +1,8 @@
+public class logical3 {
+    public static void main(String[] args){
+        boolean isloading = false;
+        boolean isloaded = true;
+        System.out.println(isloading || isloaded);
+        System.out.println(isloading | isloaded);
+    }
+}
