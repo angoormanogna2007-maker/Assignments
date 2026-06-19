a1bffca8e08b59e8509c8364cef2ae7ef364c029
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-19 16:54:54 +0530

    added compound2.java

---
+++ compound2.java
@@ -0,0 +1,11 @@
+public class compound2 {
+        public static void main(String[] args){
+            int price = 3000;
+
+            price -= 1500;   // price = 3000 - 1500 =1500
+            System.out.println(price);  //1500
+
+        }
+
+    
+}
