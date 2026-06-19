8e323f89de3dde6790addfefd6b5cc517e79593f
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-19 15:46:13 +0530

    added logical.java

---
+++ logical.java
@@ -0,0 +1,9 @@
+public class logical {
+    public static void main(String[] args){
+        boolean isopen = true;
+        boolean isclose = false;
+        System.out.println(isopen && isclose);
+
+        }
+    }
+
