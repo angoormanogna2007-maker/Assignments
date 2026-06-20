ddace5a095a589ca908d940d1d243fefc08ed00c
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-20 19:25:29 +0530

    added compounds.java

---
+++ compounds3.java
@@ -0,0 +1,23 @@
+public class compounds3 {
+    public static void main(String[] args){
+        int age =19;
+        int rent =1500;
+        int marks =60;
+        int rollNumber =13;
+        int year =2026;
+        
+        age +=19;
+        rent -=1500;
+        marks *=60;
+        rollNumber /=13;
+        year %=2026;
+
+        System.out.println(age);
+        System.out.println(rent);
+        System.out.println(marks);
+        System.out.println(rollNumber);
+        System.out.println(year);
+
+
+    }
+}
