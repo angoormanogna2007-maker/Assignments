be2621eefc800bc9ff77af53f5ddaa756907a261
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-22 17:15:27 +0530

    added compound4.java

---
+++ compound4.java
@@ -0,0 +1,17 @@
+public class compound4 {
+    public static void main(String[]args){
+        int balance = 700;
+        System.out.println(balance);
+        balance +=100;
+        System.out.println(balance);
+        balance -=200;
+        System.out.println(balance);
+        balance *=500;
+        System.out.println(balance);
+        balance /=50;
+        System.out.println(balance);
+        balance %=600;
+
+
+    }
+}
