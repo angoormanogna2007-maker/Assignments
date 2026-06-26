1daa7797236301dbe70560b3248a7cf7e6aed7e0
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-26 21:11:44 +0530

    addeded Assignment3.java

---
+++ Assignment2.java
@@ -0,0 +1,9 @@
+public class Assignment2 {
+    public static void main(String[] args){
+        marks();
+
+    }
+    public static void marks(){
+        System.out.println("marks = 90");
+    }
+}
