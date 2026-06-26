74fa686455608cfb5ea2f0221bdef4ba0668e04c
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-26 21:00:31 +0530

    added Assignments.java

---
+++ Assignments1.java
@@ -0,0 +1,20 @@
+public class Assignments1 {
+    public static void main (String[] args){
+        name();
+        RollNumber();
+    }
+        
+
+        public static void name(){
+            System.out.println("Manogna");
+        }
+
+        
+public static void RollNumber(){
+    int RollNumber = 13;
+    System.out.println(RollNumber);
+}
+    }
+
+    
+
