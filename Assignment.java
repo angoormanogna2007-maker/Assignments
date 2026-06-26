1b70df0ad3139a84cfb7cd66cf2838f5444551d8
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-26 20:45:02 +0530

    added Assignments.java

---
+++ Assignment.java
@@ -0,0 +1,12 @@
+public class Assignment {
+    public static void main(String[] args){
+         price(500);
+
+         }
+         public static void price(int price){
+            System.out.println(price);
+         }
+
+    }
+    
+
