f0baf83c6b82bd43a7ee3592c96400f75f164519
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-28 20:40:09 +0530

    Added Assignment5.java

---
+++ Assignment5.java
@@ -0,0 +1,17 @@
+public class Assignment5 {
+    public static void main(String args[]){
+        fullName("Angoor Manogna");
+        age(19);
+        college("Nishitha");
+
+    }
+    public static  void fullName(String fullName){
+        System.out.println( fullName);
+    }
+    public static void age(int age){
+        System.out.println(age);
+    }
+    public static void college(String college){
+        System.out.println(college);
+    }
+}
