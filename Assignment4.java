bcaf776d694964bcd9332b8adeeb93fb306de0a4
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-28 20:23:11 +0530

    Added Assignment4.java

---
+++ Assignment4.java
@@ -0,0 +1,21 @@
+public class Assignment4 {
+    public static void main(String args[]){
+        name();
+        RollNumber();
+        marks();
+
+    }
+    public static void name(){
+        System.out.println("manogna");
+    
+    }
+    public static void RollNumber(){
+        int RollNumber = 13;
+        System.out.println(RollNumber);
+    }
+    public static void marks(){
+        int marks = 60;
+        System.out.println(marks);
+    }
+
+}
