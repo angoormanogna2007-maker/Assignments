97dbe34fd5c8c4f93c798569e70a5c0715ec0798
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-30 12:12:57 +0530

    added conditionalStatements.java

--- conditionalStaments.java
+++
@@ -1,9 +0,0 @@
-public class conditionalStaments {
-    int score =85;
-    public static void main(String[] args){
-        if(score >= 90){
-            System.out.println()
-
-        }
-    }
-}
---
+++ conditionalStatements.java
@@ -0,0 +1,15 @@
+public class conditionalStatements {
+   static int score = 90;
+    public static void main(String[] args){
+        if (score >= 85){
+            System.out.println("Grade : A");
+        }
+        else {
+            System.out.println("Grade : B");
+        }
+
+    }
+}
+
+    
+
