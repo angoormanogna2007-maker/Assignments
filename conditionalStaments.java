e7b1168941308d9eabb19693daef766c9f88835a
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-30 11:50:04 +0530

    added return1.java

---
+++ conditionalStaments.java
@@ -0,0 +1,9 @@
+public class conditionalStaments {
+    int score =85;
+    public static void main(String[] args){
+        if(score >= 90){
+            System.out.println()
+
+        }
+    }
+}
