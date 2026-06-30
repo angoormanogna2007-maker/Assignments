44fd8860b409a08c21a9443d45631ec14cc72bf7
A MANOGNA <angoormanogna2007@gmail.com>
2026-06-30 11:48:50 +0530

    added return.java

---
+++ return1.java
@@ -0,0 +1,32 @@
+public class return1{
+
+    public static void main(String[] args){
+       
+       System.out.println(add(8,6));
+       System.out.println(sub(8,6));
+       System.out.println(mul(10, 30));
+       System .out.println(div(40, 30));
+    }
+
+
+    public static int add(int a,int b){
+    return a+b;
+
+}
+
+
+public static int sub(int a,int b){
+    return a-b;
+}
+
+public static int mul(int a,int b){
+    return a*b;
+}
+
+ 
+public static int div(int a,int b){
+    return a/b;
+}
+
+}
+
