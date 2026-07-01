caf613fc08e9d00dbb247ed26a30aafac6a1cef1
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-01 21:52:54 +0530

    Assignmentss.java

---
+++ Assignmentss.java
@@ -0,0 +1,43 @@
+public class Assignmentss {
+    public static void main(String[] args){
+
+      //Arithmetic operators
+        int a = 8;
+        int b = 10;
+
+        System.out.println("a+b =" + (a+b));
+        System.out.println("a-b =" +(a-b));
+        System.out.println("a*b =" +(a*b));
+        System.out.println("a/b =" +(a/b));
+        System.out.println("a%b =" +(a%b));
+
+    // Increment and Decrement operators
+        System.out.println("a++ =" + (a++));
+        System.out.println("++a =" + (++a));
+ 
+        System.out.println("a-- =" + (a--));   
+        System.out.println("--a =" + (--a));
+
+    // Relational operators
+        System.out.println("a==b =" + (a==b));
+        System.out.println("a!=b =" + (a!=b));
+        System.out.println("a<b =" + (a<b));
+        System.out.println("a<=b =" + (a<=b));
+        System.out.println("a>b =" + (a>b));
+        System.out.println("a>=b =" + (a>=b));
+
+
+    //Logical operators
+        System.out.println("a&&b =" + (a==b)); 
+        System.out.println("a||b =" + (a<b));
+        System.out.println("a!b =" + (a<=b));
+
+    //compound operators
+        System.out.println("a+=b =" + (a+=b));
+        System.out.println("a-=b =" + (a-=b));
+        System.out.println("a*=b =" + (a*=b));
+        System.out.println("a/=b =" + (a/=b));
+        System.out.println("a%=b =" + (a%=b));
+
+    }
+}
