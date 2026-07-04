922d37113f49291881516dbb04e0638c4829cac5
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-04 20:47:26 +0530

    added test.java

---
+++ test.java
@@ -0,0 +1,36 @@
+public class test {
+    public static void main(String[] args){
+        String day = "sunday";
+        if(day=="monday"){
+            System.out.println("It is a weekday");
+        }
+            else if(day=="tuesday"){
+                System.out.println("It is weekday");
+            }
+            else if(day=="wednesday"){
+                System.out.println(" It is weekday");
+            }
+            else if(day=="thursday"){
+                System.out.println("It is weekday");
+            }
+            else if(day=="friday"){
+                System.out.println( "It is weekay");
+            }
+            else if(day=="saturday"){
+            System.out.println("It is weekend");
+            }
+            else if(day=="sunday"){
+                System.out.println("It is weekend");
+            }
+            
+            }
+        }
+
+
+        
+
+
+
+    
+    
+
