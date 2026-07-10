016b0abf697b5e8e10e76e009a2f0e67a76f62ef
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-10 12:54:15 +0530

    added loops5.java

---
+++ loops5.java
@@ -0,0 +1,11 @@
+public class loops5 {
+        public static void main(String args[]){
+        int number =6;
+        for(int i =1 ; i<=10 ;i++){
+        System.out.println( number + " * " + i + " = " + (number *i));
+        }
+    }
+}
+    
+    
+
