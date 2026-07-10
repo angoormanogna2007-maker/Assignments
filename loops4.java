9f950cabaf75173ca7de032fae84fc9719e0e99f
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-10 12:48:16 +0530

    added loops4.java

---
+++ loops4.java
@@ -0,0 +1,11 @@
+public class loops4 {
+    public static void main(String args[]){
+        int number =5;
+        int i = 1;
+        while(i <= 10){
+            System.out.println(number + " * " + i + " = " + (number*i));
+            i++;
+
+        }
+    }
+}
