00aac1c80983c12cfaccd8fd5a65f4431e0b18d7
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-17 19:19:53 +0530

    array3.java

---
+++ array3.java
@@ -0,0 +1,19 @@
+public class array3 {
+    public static void main(String[] args){ 
+        int[] age = {17,23,19,27,25};
+        age[0] = 17;
+        age[1] = 23;
+        age[2] = 19;
+        age[3] = 27;
+        age[4] = 25;
+        age[3] = 16;
+        System.out.println(age [0]);
+        System.out.println(age [1]);
+        System.out.println(age [2]);
+        System.out.println(age [3]);
+        System.out.println(age [4]);
+
+    }
+}
+
+
