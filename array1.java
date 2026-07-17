85342cd050cf6ef95ba8638b21d7f965e4b74e64
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-17 19:07:31 +0530

    array1.java

---
+++ array1.java
@@ -0,0 +1,18 @@
+public class array1 {
+    public static void main(String[] args){ 
+        int[] age = new int[5];
+        age[0] = 23;
+        age[1] = 20;
+        age[2] = 19;
+        age[3] = 18;
+        age[4] = 25;
+        System.out.println(age [0]);
+        System.out.println(age [1]);
+        System.out.println(age [2]);
+        System.out.println(age [3]);
+        System.out.println(age [4]);
+    
+
+    }
+    
+}
