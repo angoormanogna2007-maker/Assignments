bae6efd0e93733a579cdebd4ab9080b7eca54b21
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-23 15:19:00 +0530

    added arrayyDemo3.java

---
+++ arrayyDemo3.java
@@ -0,0 +1,13 @@
+public class arrayyDemo3 {
+    
+    public static void main(String[] args){
+        String[] names ={"Akshaya", "Niharika" ,"spandhana"};
+
+
+        for(int i=0; i < names.length; i++){    
+           String name = names[i];
+            System.out.println(name);
+        }
+    }
+        
+}
