acfba3f7af9e14f68088c80bac1e443733e7cc67
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-23 14:48:30 +0530

    arrayyDemo.java

---
+++ arrayyDemo.java
@@ -0,0 +1,11 @@
+public class arrayyDemo {
+    public static void main(String[] args){
+        char[] myGrade ={'A','B','c','D'};
+
+
+        for(char i=0; i < myGrade.length; i++){    //using forloop
+           char grade = myGrade[i];
+            System.out.println(grade);
+        }
+    }
+        }
