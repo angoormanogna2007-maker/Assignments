bd1647ea31d7dd4d4b0d26da035173816190cfef
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-23 15:33:01 +0530

    arrayDemo4.java

---
+++ arrayyDemo4.java
@@ -0,0 +1,15 @@
+public class arrayyDemo4 {
+    
+    public static void main(String[] args){
+        int[][] myNumbers ={{1,2,6},{3,7,4},{6,9,8}};
+            System.out.println(myNumbers[0][1]);
+            System.out.println(myNumbers[2][2]);
+            
+            
+        }
+    }
+        
+        
+
+
+
