cf0a73e2825e06eb1b77debadc433f31fae229aa
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-23 15:05:33 +0530

    added arrayyDemo2.java

---
+++ arrayyDemo2.java
@@ -0,0 +1,10 @@
+public class arrayyDemo2 {
+    
+    public static void main(String[] args){
+        String[] names ={"Manogna", "Sowmya","Pooja"};
+        for(String name:names){   
+           
+            System.out.println(name);
+        }
+    }
+}
