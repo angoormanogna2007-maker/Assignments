eea18a9a99b447a2e556fb0250a7d145d25436da
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-29 19:02:55 +0530

    added square.java

--- square.java
+++ square.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class square {
     public static void main(String[] args){
-        Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("enter a number is : ");
          
         int num = scan.nextInt();
